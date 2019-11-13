1f5bd049c87cd7457ea94ab263d793d7bb18d6e5
Phung Hoang <phung.hoang@tripi.vn>
2019-11-13 14:07:26 +0700

    OK

--- samples/02.echo-bot/src/main/java/com/microsoft/bot/sample/echo/MessageController.java
+++ samples/02.echo-bot/src/main/java/com/microsoft/bot/sample/echo/MessageController.java
@@ -1,9 +1,7 @@
 package com.microsoft.bot.sample.echo;
 
-import com.microsoft.bot.builder.Bot;
 import com.microsoft.bot.integration.BotFrameworkHttpAdapter;
 import com.microsoft.bot.integration.Configuration;
-import com.microsoft.bot.integration.spring.BotController;
 import com.microsoft.bot.schema.ConversationReference;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -20,17 +18,15 @@ import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class MessageController {
-    private Logger logger = LoggerFactory.getLogger(BotController.class);
+    private Logger logger = LoggerFactory.getLogger(MessageController.class);
 
     private final BotFrameworkHttpAdapter adapter;
-    private final Bot bot;
     private ConversationReferences conversationReferences;
     private String appId;
 
 
-    public MessageController(BotFrameworkHttpAdapter withAdapter, Bot withBot, Configuration withConfiguration, ConversationReferences withReferences) {
+    public MessageController(BotFrameworkHttpAdapter withAdapter, Configuration withConfiguration, ConversationReferences withReferences) {
         adapter = withAdapter;
-        bot = withBot;
         this.conversationReferences = withReferences;
         appId = withConfiguration.getProperty("MicrosoftAppId");
         if (StringUtils.isEmpty(appId)) {
@@ -41,12 +37,16 @@ public class MessageController {
     @GetMapping("/api/send")
     public CompletableFuture<ResponseEntity<Object>> incoming(@RequestParam String msg) {
         String message = Optional.ofNullable(msg).orElse("---");
-        for (ConversationReference reference : conversationReferences.values()) {
-            adapter.continueConversation(appId, reference, turnContext -> turnContext.sendActivity(message)
-                    .thenApply(resourceResponse -> null));
-        }
-
+        logger.info("send : {}", message);
         return CompletableFuture.supplyAsync(() -> {
+            try {
+                for (ConversationReference reference : conversationReferences.values()) {
+                    adapter.continueConversation(appId, reference, turnContext -> turnContext.sendActivity(message)
+                            .thenApply(resourceResponse -> null));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             return new ResponseEntity<>("sucessss", HttpStatus.OK);
         });
     }
